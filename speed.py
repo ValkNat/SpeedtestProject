ed2169dd14a5b61d3bc9ada8ebe35130bdbdd533
Natalie <132787852+ValkNat@users.noreply.github.com>
2023-08-21 14:56:40 -0500

    V2.1 - Bug issues fixed

--- speed.py
+++ speed.py
@@ -7,15 +7,17 @@ class TestSpeed:
     def testSpeed():
         download_speed = f"Download speed: {int(st.download()) / (1024 * 1024)} Mb/s"
         upload_speed = f"Upload speed: {int(st.upload()) / (1024 * 1024)} Mb/s"
+        combined_data = f"{download_speed} | {upload_speed}"
         print(download_speed)
         print(upload_speed)
         log_to_file = input("Log to file? [y/n]: ")
         if log_to_file == 'y':
-            TestSpeed.logToFile(download_speed, upload_speed)
+            TestSpeed.logToFile(combined_data)
         elif log_to_file == 'n':
             print("Not saving to CSV...")
         else:
             print("Format error!")
+
     def testPing():
         servers = []
         st.get_best_server()
@@ -32,10 +34,9 @@ class TestSpeed:
     def logToFile(*data):
         parameters = []
         with open('speed_data.csv', mode='a', encoding='UTF-8', errors='strict', buffering=1) as file:
-            for i in data:
-                parameters.append(i)
-            for i in parameters:
-                file.write(i)
+            writer = csv.writer(file)
+            writer.writerow(data)
+            
 
 
 test_being_run = int(input("Would you like to test: speed (1) or ping (2)? "))
